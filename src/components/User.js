aea23d7485da05a180ea60aff7c52aa42fdb5b17
Aditya Agarwal <adityaa803@gmail.com>
2019-03-09 12:04:36 +0530

    slides update

--- src/components/User.js
+++ src/components/User.js
@@ -74,22 +74,3 @@ class User extends Component {
 }
 
 export { User };
-
-/**
- * Naruto Wallpapers
-  
-    https://i.ytimg.com/vi/4jAaeL1G2vQ/maxresdefault.jpg
-
-    https://i2.wp.com/ramenswag.com/wp-content/uploads/2016/12/naruto-wallpaper.jpg?resize=1060%2C663
-    
-    http://images6.fanpop.com/image/photos/37300000/Naruto-Wallpaper-stella2015-and-redwolf279-37342668-1600-1200.jpg
-    
-    http://s1.picswalls.com/wallpapers/2015/09/27/hd-naruto_104041723_274.jpg
-
-
-  * Sasuke Wallpapers
-
-    https://img7.androidappsapk.co/poster/8/1/2/com.andromo.dev660614.app702378_2.png
-
-    http://images5.fanpop.com/image/photos/29200000/Sasuke-naruto-shippuuden-sasuke-lovers-29284988-500-400.jpg
-*/
